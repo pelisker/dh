cec5e52ba34d5e5db1d47c8e9530bd73fc773322
pelisker <pelisker@users.noreply.github.com>
2022-10-16 19:32:58 +0300

    Изменения

---
+++ Python/TestDev/urlget.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import json
+import pyperclip
+import requests
+
+
+if len(sys.argv) == 1:
+    param_url = sys.argv[1]
+
+#    if param_json[0] in ('"', "'"):
+        #pyperclip.copy('В буфере обмена должен содержаться словарь jsonData без внешних ковычек')
+#        exit(1)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'charset': 'UTF-8'
+    }
+
+#    data = json.dumps(param_json)
+ #   data = json.loads(data)
+
+    try:
+        resp = requests.get(url=param_url)
+                            #, headers=headders, auth=(param_key1, param_key2), data=data.encode('utf-8')
+
+        if resp.status_code == 200:
+            res = json.loads(resp.text)
+            pyperclip.copy(res['Model']['Url'])
+            exit(0)
+        else:
+            pyperclip.copy(resp.status_code, resp.raise_for_status())
+    except requests.exceptions.RequestException as err:
+        pyperclip.copy(err)
+        exit(1)
+else:
+    pyperclip.copy("Ошибка. Не передан url.")
+    exit(1)
+
